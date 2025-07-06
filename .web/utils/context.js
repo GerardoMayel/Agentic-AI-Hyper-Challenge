25a0e35d839aa5efb28e600286723e50fbc965f2
Gerardo Mayel Fernandez Alamilla <mayelmacbookm4pro@Gerardos-MacBook-Pro.local>
2025-07-05 22:14:27 -0600

    se implmenta url dinamica

--- .web/utils/context.js
+++ .web/utils/context.js
@@ -22,7 +22,7 @@ export const initialEvents = () => []
 
 export const isDevMode = true
 
-export const lastCompiledTimeStamp = "2025-07-04 21:28:02.110982"
+export const lastCompiledTimeStamp = "2025-07-05 22:13:21.202601"
 
 export function UploadFilesProvider({ children }) {
   const [filesById, setFilesById] = useState({})
